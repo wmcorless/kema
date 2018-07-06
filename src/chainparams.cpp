0d52a22e2117d1d2dc566ed7a2359ef2f35d9fc8
William Corless <wmcorless@gmail.com>
2018-07-05 21:38:58 -0700

    fixes

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -133,7 +133,7 @@ public:
         hashGenesisBlock = genesis.GetHash();
 	//printf("%s\n", hashGenesisBlock.ToString().c_str());
 	//printf("%s\n", genesis.hashMerkleRoot.ToString().c_str());
-
+    printf("%s\n", genesis.nBits.ToString().c_str());
 
         assert(hashGenesisBlock == uint256("0xc2ba69bb5a1861d5bb8f8b4426652d7fff7b3b7db0bf05271e9b70b1bc5fb682"));
         assert(genesis.hashMerkleRoot == uint256("0xfbbfadf1a74fe46a60011a392709d7d99d38202fc6d4d84aa211581ded64fef6"));
