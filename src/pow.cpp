6f8d951365dcb8e60e8c748318772fa6bb9cea78
William Corless <wmcorless@gmail.com>
2018-07-22 11:31:42 -0700

    pow

--- src/pow.cpp
+++ src/pow.cpp
@@ -49,14 +49,18 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
         // ppcoin: target change every block
         // ppcoin: retarget with exponential moving toward target spacing
         uint256 bnNew;
-
+        if(pindexLast->nHeight >= Params().LAST_POW_BLOCK() && pindexLast->nHeight <= Params().LAST_POW_BLOCK() + 2) {
+			LogPrintf("DarkGravityWave: drop difficulty in PoS start\n");
+			uint256 bnTargetZero = (~uint256(0) >> 4);
+			bnNew = bnTargetZero;
+        } else
         bnNew.SetCompact(pindexLast->nBits);
 
         int64_t nInterval = nTargetTimespan / nTargetSpacing;
         bnNew *= ((nInterval - 1) * nTargetSpacing + nActualSpacing + nActualSpacing);
         bnNew /= ((nInterval + 1) * nTargetSpacing);
 
-		if (pindexLast->nHeight == Params().LAST_POW_BLOCK() || pindexLast->nHeight <= Params().LAST_POW_BLOCK() +2 ) bnNew = bnTargetLimit;
+//		if (pindexLast->nHeight == Params().LAST_POW_BLOCK() || pindexLast->nHeight <= Params().LAST_POW_BLOCK() +2 ) bnNew = bnTargetLimit;
         if (bnNew <= 0 || bnNew > bnTargetLimit)
             bnNew = bnTargetLimit;
         return bnNew.GetCompact();
@@ -120,6 +124,11 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
     return bnNew.GetCompact();
 }
 
+unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader* pblock)
+{
+	return DarkGravityWave(pindexLast);
+}
+
 bool CheckProofOfWork(uint256 hash, unsigned int nBits)
 {
     bool fNegative;
@@ -136,8 +145,8 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits)
         return error("CheckProofOfWork() : nBits below minimum work");
 
     // Check proof of work matches claimed amount
-    if (hash == bnTarget)
-		return error("CheckProofOfWork() : hash doesn't match nBits");
+    //if (hash == bnTarget)
+	//	return error("CheckProofOfWork() : hash doesn't match nBits");
 
     return true;
 }
