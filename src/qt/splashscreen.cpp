ee533db385619d6ff0601aeb2078fa7eee71604c
William Corless <wmcorless@gmail.com>
2018-08-05 02:21:07 -0700

    fixes

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -34,11 +34,11 @@ SplashScreen::SplashScreen(Qt::WindowFlags f, const NetworkStyle* networkStyle) 
     float fontFactor = 1.0;
 
     // define text to place
-    QString titleText = tr("kema Coin");
-    QString versionText = tr("v1.0");
+    QString titleText = tr("Kema Coin");
+    QString versionText = tr("v1.0.0.1");
     QString copyrightTextBtc = QChar(0xA9) + QString(" 2009-%1 ").arg(COPYRIGHT_YEAR) + QString(tr("The Bitcoin Core developers"));
     QString copyrightTextDash = QChar(0xA9) + QString(" 2014-%1 ").arg(COPYRIGHT_YEAR) + QString(tr("The Dash Core and PIVX developers"));
-    QString copyrightTextkema = QChar(0xA9) + QString(" 2018-%1 ").arg(COPYRIGHT_YEAR) + QString(tr("The kema developers"));
+    QString copyrightTextkema = QChar(0xA9) + QString(" 2018-%1 ").arg(COPYRIGHT_YEAR) + QString(tr("The Kema Coin developers"));
     QString titleAddText = networkStyle->getTitleAddText();
     QString font = QApplication::font().toString();
 
